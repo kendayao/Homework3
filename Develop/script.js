fcc8bfbbdefbae8bf4fc2ddc684f5378112c422b
Kaeneth Dayao <kendayao@gmail.com>
2020-04-22 19:19:07 -0700

    added function and conditionals to js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+    var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var numeric = ["0","1","2","3","4","5","6","7","8","9"]
+    var specialChar = ["!","#","$","%","$","&","(",")","*","+","-","/",":",";","<","=",">","@","?","{","}","[","]"]
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var characterCount = prompt("How many characters do you want in your password 8-128")
+  var lowerCaseConfirm = confirm("Do you want lower case letters?")
+  var upperCaseConfirm = confirm ("Do you want upper case letters?")
+  var numericConfirm = confirm("Do you want any numbers?")
+  var specialConfirm = confirm("Do you want any special characters?")
+
+ 
+    
+    var newPassword= ""
+      function generatePassword() {
+      if (lowerCaseConfirm===true && upperCaseConfirm===false && numericConfirm===false && specialConfirm===false){
+        for (var i=0; i < characterCount; i++){
+          var characterPosition=Math.floor(Math.random()*(lowerCase.length))
+           newPassword += lowerCase[characterPosition]
+           }
+         }else if (lowerCaseConfirm===false && upperCaseConfirm===true && numericConfirm===false && specialConfirm===false){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(upperCase.length))
+          newPassword += upperCase[characterPosition]
+          }
+        }else if (lowerCaseConfirm===false && upperCaseConfirm===f && numericConfirm===true && specialConfirm===false){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(numeric.length))
+          newPassword += numeric[characterPosition]
+          }
+        }else if (lowerCaseConfirm===false && upperCaseConfirm===false && numericConfirm===false && specialConfirm===true){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(specialChar.length))
+          newPassword += specialChar[characterPosition]
+          }
+        }else if (lowerCaseConfirm===true && upperCaseConfirm===true && numericConfirm===false && specialConfirm===false){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(upperCase.length + lowerCase.length))
+          newPassword += lowerCase.concat(upperCase)[characterPosition]
+          }
+        }else if (lowerCaseConfirm===true && upperCaseConfirm===false && numericConfirm===true && specialConfirm===false){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(numeric.length + lowerCase.length))
+          newPassword += lowerCase.concat(numeric)[characterPosition]
+          }
+        }else if (lowerCaseConfirm===true && upperCaseConfirm===false && numericConfirm===false && specialConfirm===true){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(specialChar.length + lowerCase.length))
+          newPassword += lowerCase.concat(specialChar)[characterPosition]
+          }
+        }else if (lowerCaseConfirm===true && upperCaseConfirm===true && numericConfirm===true && specialConfirm===false){
+          for (var i=0; i < characterCount; i++){
+           var characterPosition=Math.floor(Math.random()*(upperCase.length + lowerCase.length + numeric.length))
+          newPassword += lowerCase.concat(upperCase,numeric)[characterPosition]
+          }
+        }
+      
+
+
+
+
+
+
+       
+      }
+  generatePassword();
+      
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newPassword;
 
 }
 
@@ -15,21 +82,41 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-    var characterCount = prompt("How many characters do you want in your password 8-128")
+    // var characterCount = prompt("How many characters do you want in your password 8-128")
+    // var lowerCaseConfirm = confirm("Do you want lower case letters?")
+    // var upperCaseConfirm = confirm ("Do you want upper case letters?")
+    // var numericConfirm = confirm("Do you want any numbers?")
+    // var specialConfirm = confirm("Do you want any special characters?")
 
-    var lowerCaseConfirm = confirm("Do you want lower case letters?")
-    
-    var upperCaseConfirm = confirm ("Do you want upper case letters?")
-   
-    var numericConfirm = confirm("Do you want any numbers?")
+    // var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    // var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    // var numeric = ["0","1","2","3","4","5","6","7","8","9"]
+    // var specialChar = ["!","#","$","%","$","&","(",")","*","+","-","/",":",";","<","=",">","@","?","{","}","[","]"]
+
+
+    // var newPassword="";
+
+    //   if (lowerCaseConfirm===true && upperCaseConfirm===false && numericConfirm===false && specialConfirm===false){
+    //     for (var i=0; i < characterCount; i++){
+    //       var characterPosition=Math.floor(Math.random()*(lowerCase.length))
+    //       newPassword = newPassword + lowerCase[characterPosition]
+    //       }
+    //       alert(newPassword);
+    //     }
+       
+
+    //   else if (lowerCaseConfirm===false && upperCaseConfirm===true && numericConfirm===false && specialConfirm===false){
+    //       for (var i=0; i < characterCount; i++){
+    //         var characterPosition=Math.floor(Math.random()*(upperCase.length))
+    //         newPassword = newPassword + upperCase[characterPosition]
+    //        }
+    //        alert(newPassword);
+
+    //       }
+         
   
-    var specialConfirm = confirm("Do you want any special characters?")
-   
-    
-    var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    var numeric = ["0","1","2","3","4","5","6","7","8","9"]
-    var special = ["!","#","$","%","$","&","(",")","*","+","-","/",":",";","<","=",">","@","?","{","}","[","]"]
+
+
 
    
 
